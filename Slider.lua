9ad8e02dd5e293180157e2b7fe5498b77e3290e4
Diego Martínez <kaeza@users.noreply.github.com>
2017-07-13 00:48:55 -0300

    Slider: Add support for anchor points.

--- Slider.lua
+++ Slider.lua
@@ -31,16 +31,21 @@ Slider.value = 0
 Slider.increment = nil
 
 ---
--- Color for the light parts of the handle.
+-- Anchor border.
 --
--- @tfield simpleui.Color handlelightcolor Default is (255, 255, 255).
-Slider.handlelightcolor = ({ 255, 255, 255 })
+-- If it is `"l"`, the value increments left to right.
+-- If it is `"r"`, the value increments right to left.
+-- If it is `"t"`, the value increments top to bottom.
+-- If it is `"b"`, the value increments bottom to top.
+--
+-- @tfield string anchor Default is "l".
+Slider.anchor = "l"
 
 ---
--- Color for the dark parts of the handle.
+-- Color for the handle while pressed.
 --
--- @tfield simpleui.Color handledarkcolor Default is (255, 255, 255).
-Slider.handledarkcolor = ({ 0, 0, 0 })
+-- @tfield simpleui.Color handlecolorpressed Default is (192, 192, 192).
+Slider.handlecolorpressed = ({ 192, 192, 192 })
 
 ---
 -- Set the slider's value.
@@ -66,23 +71,31 @@ end
 -- The handle is part of the "foreground".
 --
 -- @tparam number x X position of the center of the handle.
+-- @tparam number y Y position of the center of the handle.
+-- @tparam boolean vert True if this is a vertical bar, false otherwise.
+-- @tparam boolean pressed True if the handle is currently pressed,
+--  false otherwise.
 -- @see simpleui.Widget:paintfg
-function Slider:painthandle(x)
-	local size = self.h/6
-	gfx.setColor(self.handlelightcolor)
-	gfx.polygon("fill", x, size, x-size, 0, x+size, 0)
-	gfx.setColor(self.handledarkcolor)
-	gfx.polygon("line", x, size, x-size, 0, x+size, 0)
-	gfx.setColor(self.handlelightcolor)
-	gfx.polygon("fill", x, size*5, x-size, size*6, x+size, size*6)
-	gfx.setColor(self.handledarkcolor)
-	gfx.polygon("line", x, size*5, x-size, size*6, x+size, size*6)
-	gfx.setColor(self.handlelightcolor)
-	gfx.line(x, size, x, size*2)
-	gfx.line(x, size*3, x, size*4)
-	gfx.setColor(self.handledarkcolor)
-	gfx.line(x, size*2, x, size*3)
-	gfx.line(x, size*4, x, size*5)
+function Slider:painthandle(x, y, vert, pressed)
+	local size = (vert and self.w or self.h)/4
+	local fg = (self.enabled
+			and (pressed and self.handlecolorpressed or self.fgcolor)
+			or self.fgcolordisabled)
+	local bc = self.bordercolor
+	gfx.push()
+	gfx.translate(x, y)
+	if vert then
+		gfx.rotate(math.pi/2)
+	end
+	gfx.setColor(fg)
+	--                 | P1        | P2              | P3
+	gfx.polygon("fill", 0, -size, -size, -size*2, size, -size*2)
+	gfx.polygon("fill", 0,  size, -size,  size*2, size,  size*2)
+	gfx.line(0, -size, 0, size)
+	gfx.setColor(bc)
+	gfx.polygon("line", 0, -size, -size, -size*2, size, -size*2)
+	gfx.polygon("line", 0,  size, -size,  size*2, size,  size*2)
+	gfx.pop()
 end
 
 ---
@@ -105,7 +118,11 @@ function Slider:valuechanged()
 end
 
 function Slider:calcminsize()
-	return 128, 16
+	if self.anchor == "t" or self.anchor == "b" then
+		return 16, 32
+	else
+		return 32, 16
+	end
 end
 
 function Slider:mousepressed(x, _, b)
@@ -121,9 +138,19 @@ function Slider:mousereleased(_, _, b)
 	end
 end
 
-function Slider:mousemoved(x)
+function Slider:mousemoved(x, y)
 	if self._pressed then
-		local v = x/self.w
+		local anchor = self.anchor
+		local v
+		if anchor == "t" then
+			v = y/self.h
+		elseif anchor == "b" then
+			v = 1-y/self.h
+		elseif anchor == "r" then
+			v = 1-x/self.w
+		else
+			v = x/self.w
+		end
 		if self.increment then
 			v = floor(v/self.increment+.5)*self.increment
 		end
@@ -137,7 +164,15 @@ function Slider:wheelmoved(_, y)
 end
 
 function Slider:paintfg()
-	self:painthandle(self.w*self.value)
+	local anchor = self.anchor
+	local vert = anchor == "t" or anchor == "b"
+	local invert = anchor == "r" or anchor == "b"
+	local x = vert and self.w/2 or self.w*self.value
+	local y = vert and self.h*self.value or self.h/2
+	if invert then
+		x, y = self.w-x, self.h-y
+	end
+	self:painthandle(x, y, vert, self._pressed)
 	Widget.paintfg(self)
 end
 
